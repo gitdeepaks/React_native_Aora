859336d6cb656153a5f31f792a7811b2d8808e77
gitdeepaks <deepaksankhyan92@outlook.com>
2024-04-12 21:10:35 +0530

    initial 🚀

---
+++ app/index.jsx
@@ -0,0 +1,21 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../contants";
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image source={images.logo}></Image>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
